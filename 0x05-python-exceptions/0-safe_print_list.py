7158651f155107aaa21138f162e8079ca38387ab
Francis <okwirfrancis2010@gmail.com>
2022-12-19 22:29:02 +0300

    Added all task file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,11 +3,9 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end='')
-        except:
-            break
-        else:
-            count += 1
-
-    print()
-    return (count)
+            print("{}".format(my_list[i]), end="")
+            count = count + 1
+        except IndexError:
+            continue
+    print("")
+    return count
